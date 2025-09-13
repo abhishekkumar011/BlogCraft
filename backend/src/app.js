b043fad18ef5600d14412f3acc24ab9de71885c7
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-09-14 02:00:00 +0530

    feat(API) : create login and signup user endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,61 @@
+import { User } from "../models/user.model.js";
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    const existedUser = await User.findOne({ email });
+
+    if (existedUser) {
+      return res
+        .status(409)
+        .json({ msg: "User with this email already exist" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(200).json({ user, msg: "User registered Successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return res
+      .status(500)
+      .json({ msg: "Something went wrong while registering the user" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(409).json({ msg: "User does not exists" });
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ msg: "Invalid user credentials" });
+    }
+
+    return res.status(200).json({ user, msg: "User logged in Successfully" });
+  } catch (error) {
+    console.error("Error while logging user ", error);
+    return res.status(500).json({ msg: "Error while logging the user" });
+  }
+};
+
+export { signup, login };
