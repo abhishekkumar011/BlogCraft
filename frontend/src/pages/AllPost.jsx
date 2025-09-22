e3a80c7f41cd35f80062c6c66ccc8c01eac9eba7
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-09-22 23:34:35 +0530

    style: responsive all post page and refactor some code

--- frontend/src/pages/AllPost.jsx
+++ frontend/src/pages/AllPost.jsx
@@ -7,28 +7,42 @@ function AllPost() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    try {
+    const fetchPosts = async () => {
       setLoading(true);
-      const fetchPosts = async () => {
+      try {
         const response = await axios.get(
           `${import.meta.env.VITE_API_URL}/posts/`
         );
         setPosts(response.data.posts);
-      };
-      fetchPosts();
-    } catch (error) {
-      console.error("Could not load posts ", error);
-    } finally {
-      setLoading(false);
-    }
+      } catch (error) {
+        console.error("Could not load posts ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="p-5 md:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div
+            key={index}
+            className="bg-white shadow rounded-lg p-4 animate-pulse space-y-4"
+          >
+            <div className="h-40 bg-gray-200 rounded-md"></div>
+            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
-    <div className="p-10 grid grid-cols-4">
+    <div className="p-5 md:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
       {posts.length === 0 ? (
         <div className="text-center text-2xl w-full">There is no post</div>
       ) : (
