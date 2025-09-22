1f7c8a5b7d88155139bb0d0e9383635104aafd1c
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-09-23 00:34:13 +0530

    style: responsive post form page

--- frontend/src/components/PostFormComponent.jsx
+++ frontend/src/components/PostFormComponent.jsx
@@ -49,22 +49,22 @@ function PostFormComponent() {
   };
 
   return (
-    <div className="p-10 flex items-center justify-center">
-      <div className="p-10 shadow-md w-2/3 rounded-lg border border-gray-300">
-        <h4 className="text-primary text-2xl font-semibold">
+    <div className="py-10 px-5 md:px-0 flex items-center justify-center">
+      <div className="p-5 md:p-10 shadow-md w-full md:w-2/3 rounded-lg border border-gray-300">
+        <h4 className="text-primary text-xl md:text-2xl font-semibold text-center">
           Create Your Post
         </h4>
 
         <form onSubmit={handleSubmit}>
           <div className="mt-10 flex flex-col gap-8">
             <div className="flex flex-col gap-1">
-              <label className=" font-medium text-gray-800">Post Title</label>
+              <label className="font-medium text-gray-800">Post Title</label>
               <input
                 required
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
                 placeholder="Enter your title"
-                className="border-2 border-gray-200 p-2 rounded-md outline-0"
+                className="border-2 border-gray-200 text-sm p-2 rounded-md outline-0"
               />
             </div>
 
@@ -73,7 +73,7 @@ function PostFormComponent() {
               <textarea
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
-                className="border-2 h-50 border-gray-200 p-2 rounded-md outline-0"
+                className="border-2 h-50 border-gray-200 text-sm p-2 rounded-md outline-0"
                 required
                 placeholder="Enter your content"
               />
@@ -85,17 +85,17 @@ function PostFormComponent() {
               </p>
             )}
 
-            <div className="flex justify-between">
+            <div className="flex flex-col md:flex-row items-center md:justify-between gap-4">
               <input
                 type="file"
-                className="font-medium text-primary border border-gray-300 rounded p-2 w-fit cursor-pointer"
+                className="font-medium text-primary border border-gray-300 rounded p-2 w-full md:w-auto cursor-pointer text-sm"
                 accept="image/*"
                 onChange={(e) => setFile(e.target.files[0])}
               />
 
               <button
                 type="submit"
-                className="flex items-center gap-2 text-white bg-primary px-4 py-2 w-fit rounded-md cursor-pointer"
+                className="flex items-center gap-2 text-white bg-primary px-4 py-2 w-full md:w-auto justify-center rounded-md cursor-pointer text-sm md:text-base"
               >
                 Publish
                 <Upload size={18} />
--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
+import { Pencil, Trash2 } from "lucide-react";
+import { AuthContext } from "../context/AuthContext";
 import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import { Pencil, Trash2 } from "lucide-react";
 
 function Post() {
   const { id } = useParams();
@@ -13,20 +13,20 @@ function Post() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    try {
+    const fetchSinglePost = async (id) => {
       setLoading(true);
-      const fetchSinglePost = async (id) => {
+      try {
         const response = await axios.get(
           `${import.meta.env.VITE_API_URL}/posts/p/${id}`
         );
         setPost(response.data.postById);
-      };
-      fetchSinglePost(id);
-    } catch (error) {
-      console.error("Could not load post ", error);
-    } finally {
-      setLoading(false);
-    }
+      } catch (error) {
+        console.error("Could not load post ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSinglePost(id);
   }, []);
 
   const handleDeletePost = async () => {
@@ -47,16 +47,26 @@ function Post() {
   };
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="p-5 md:p-0 max-w-3xl mx-auto">
+        <div className="bg-white shadow rounded-lg p-4 animate-pulse space-y-4">
+          <div className="h-40 bg-gray-200 rounded-md"></div>
+          <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+        </div>
+      </div>
+    );
   }
 
   const isAuthor = user && post.author && user._id === post.author._id;
 
   return (
     <div className="flex justify-center">
-      <div className="rounded-md shadow-md mx-20 my-10 py-10 px-10 border border-gray-300 space-y-4 max-w-3xl">
-        <h1 className="text-4xl font-semibold">{post?.title}</h1>
-        <div className="flex items-center justify-between">
+      <div className="rounded-md shadow-md mx-5 md:mx-20 my-10 py-10 px-5 md:px-10 border border-gray-300 space-y-4 w-full md:max-w-3xl">
+        <h1 className="text-2xl text-center md:text-left md:text-4xl font-semibold">
+          {post?.title}
+        </h1>
+        <div className="flex flex-col sm:flex-row gap-3 sm:gap-0 items-center justify-between">
           <div className="flex items-center gap-2">
             <div className="w-8 h-8 rounded-full overflow-hidden">
               <img src="https://github.com/shadcn.png" />
@@ -73,10 +83,10 @@ function Post() {
         <p className="text-gray-500 text-sm line-clamp-3">{post?.content}</p>
 
         {isAuthor && (
-          <div className="flex justify-between">
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between">
             <Link
               to={`/post/edit/${post._id}`}
-              className="flex items-center gap-2 bg-primary hover:bg-primary/80 text-white px-4 py-2 rounded-md font-medium"
+              className="flex items-center justify-center gap-2 bg-primary hover:bg-primary/80 text-white px-4 py-2 rounded-md font-medium"
             >
               Edit
               <Pencil size={17} />
@@ -84,7 +94,7 @@ function Post() {
 
             <button
               onClick={handleDeletePost}
-              className="flex items-center gap-2 bg-red-600 cursor-pointer hover:bg-red-500 text-white px-4 py-2 rounded-md font-medium"
+              className="flex items-center justify-center gap-2 bg-red-600 cursor-pointer hover:bg-red-500 text-white px-4 py-2 rounded-md font-medium"
             >
               Delete
               <Trash2 size={17} />
