b30866f601b9eef77a72d5a93e2b37b3deae619b
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-09-22 22:25:08 +0530

    style: responsive hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -3,25 +3,25 @@ import { ArrowRight, Sparkles } from "lucide-react";
 
 function HeroSection() {
   return (
-    <div className="flex flex-col justify-center items-center py-20">
-      <div className="flex items-center px-4 py-2 rounded-full bg-primary/10 text-primary text-sm font-medium mb-6">
+    <div className="flex flex-col justify-center items-center py-20 px-5 md:px-0">
+      <div className="flex items-center px-4 py-2 rounded-full bg-primary/10 text-primary text-xs  md:text-sm font-medium mb-6">
         <Sparkles className="h-4 w-4 mr-2" />
         Welcome to the future of blogging
       </div>
 
-      <h1 className="text-6xl font-bold mb-6 text-center text-gray-800">
+      <h1 className="text-5xl md:text-6xl font-bold mb-6 text-center text-gray-800">
         Share Your Stories
         <br />
         <span className="text-primary">Inspire the World</span>
       </h1>
 
-      <p className="text-xl text-gray-500 mb-8 max-w-2xl text-center">
+      <p className="md:text-xl text-gray-500 mb-8 max-w-2xl text-center">
         Join thousands of writers and creators on BlogCraft. Create beautiful
         posts, build your audience, and connect with readers who love your
         content.
       </p>
 
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center flex-col md:flex-row gap-4">
         <Link to="/createpost">
           <button className="bg-primary hover:bg-primary/80 cursor-pointer flex items-center px-4 py-2 rounded-lg text-white">
             Start Writing Today
